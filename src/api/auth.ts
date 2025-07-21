76c02f1e6dca002e6bcaea52c4ff76b1d64792f4
kene-creator <keneagagwuu@gmail.com>
2025-07-21 14:32:47 +0100

    update: include credentials in the brower cookie automatically

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -26,6 +26,7 @@ export async function login({
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify({ email, password }),
   });
 
