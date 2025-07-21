8233624713684c6c13a8e1bd2b10a7935d42df13
kene-creator <keneagagwuu@gmail.com>
2025-07-21 13:56:12 +0100

    update: add vercel file

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
+import Login from "./pages/Login.vue";
+import Dashboard from "./pages/Dashboard.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/login",
     name: "Login",
-    component: () => import("./pages/Login.vue"),
+    component: Login,
   },
   {
     path: "/dashboard",
     name: "Dashboard",
-    component: () => import("./pages/Dashboard.vue"),
+    component: Dashboard,
   },
 ];
 
